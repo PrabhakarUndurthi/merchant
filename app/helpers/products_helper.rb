8e459115c2a137f34a2f4d8c5a96234c53da0003
Prabhakar Undurthi <undurthi_prabhakar@aol.com>
2014-12-30 15:33:48 +0530

    Add stock option to the products.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,10 @@ module ProductsHelper
   def print_price(price)
     "$#{price}"
   end
+
+  def print_stock(stock)
+    if stock > 0
+    else
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  validates_numericality_of :price
+  validates_presence_of :stock, numericality: { only_integer: true }
 end
